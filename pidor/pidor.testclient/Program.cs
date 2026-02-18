980353c9cfec6c6fa6d4de538cf02a2033366e9d
sp1ritas <sp1ritas@tutamail.com>
2026-02-19 00:56:42 +0200

    pydarast

---
+++ pidor/pidor.testclient/Program.cs
@@ -0,0 +1,12 @@
+﻿using pidor.Network;
+
+class Program
+{
+    static async Task Main()
+    {
+        var client = new TcpSecureClient();
+        await client.ConnectAsync("127.0.0.1", 1337);
+
+        Console.ReadLine();
+    }
+}
